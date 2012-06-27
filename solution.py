13f654a4461b7947326f76a894b5afa213dcd9dd
nooby <marinnikolaev61306@gmail.com>
2012-06-27 13:25:11 +0300

    all functions are implemented need some documentation and formating of code

--- solution.py
+++ solution.py
@@ -1,99 +1,150 @@
-import pygame, sys, Enemy
+import pygame, sys, Enemy, shelve
 from pygame.locals import *
 
-pygame.font.init()
-running = 1
-pressed_down=False
-strike=False
-time=0
-hits=0
-
-screen = pygame.display.set_mode((600, 600));
-enemy_image = pygame.image.load("KS_H_SM.bmp").convert()
-enemy_image.set_colorkey((180,180,180))
-cursor = pygame.image.load("Cursor.bmp").convert()
-cursor.set_colorkey((180,180,180))
-enemy_dead = pygame.image.load("KS_SAD_SM.bmp").convert()
-enemy_dead.set_colorkey((180,180,180))
-background = pygame.image.load("Field.jpg").convert()
-bang = pygame.image.load("Bang!.bmp").convert()
-bang.set_colorkey((255,255,255))
-clock=pygame.time.Clock()
-enemy1= Enemy.Enemy(160,140,1000,3000,False,enemy_image)
-enemy2= Enemy.Enemy(351,140,2500,4500,False,enemy_image)
-enemy3= Enemy.Enemy(355,401,4000,6500,False,enemy_image)
-enemy4= Enemy.Enemy(200,401,6500,8000,False,enemy_image)
-enemy5= Enemy.Enemy(258,275,8500,10500,False,enemy_image)
-enemy6= Enemy.Enemy(460,527,10000,12000,False,enemy_image)
-enemy7= Enemy.Enemy(45,528,12500,13500,False,enemy_image)
-enemy8= Enemy.Enemy(255,530,13000,15000,False,enemy_image)
-enemy9= Enemy.Enemy(450,94,14500,16500,False,enemy_image)
-enemy10= Enemy.Enemy(62,94,16000,18000,False,enemy_image)
-enemy11= Enemy.Enemy(242,94,18800,20000,False,enemy_image)
-pygame.mouse.set_visible(0)
-enemies=[enemy1,enemy2,enemy3,enemy4,enemy5,enemy6,enemy7,enemy8,enemy9,enemy10,enemy11]
-
-
-def strike_checker(mouse_click_coordinates, x_of_image, y_of_image):
-    if (mouse_click_coordinates[0] > x_of_image and mouse_click_coordinates[0] < x_of_image+50 and 
-        mouse_click_coordinates[1] > y_of_image and mouse_click_coordinates[1] < y_of_image + 70):
-        return True
+class Game(object):
+    def __init__(self,screen,enemy,enemy_dead,field,sound):
+        self.screen = screen
+        self.setup_initial_variables(sound)
+        self.setup_images(enemy,enemy_dead,field)
+        self.setup_enemies()
         
-def clear_screen():
-    screen.blit(background, (0,0))
+        
+    def setup_initial_variables(self,sound):
+         pygame.font.init()
+         pygame.mixer.init()
+         self.running = 1
+         self.pressed_down=False
+         self.strike=False
+         self.time=0
+         self.hits=0
+         self.clock=pygame.time.Clock()
+         pygame.mouse.set_visible(0)
+         self.sound=pygame.mixer.Sound(sound)
+         self.sound_smash=pygame.mixer.Sound('smash.wav')
+         
+         
+    def setup_images(self,enemy,enemy_dead,field):
+        self.enemy_image = pygame.image.load(enemy).convert()
+        self.enemy_image.set_colorkey((180,180,180))
+        self.cursor = pygame.image.load("capture.png").convert()
+        self.cursor.set_colorkey((0,0,0))
+        self.enemy_dead = pygame.image.load(enemy_dead).convert()
+        self.enemy_dead.set_colorkey((180,180,180))
+        self.background = pygame.image.load(field).convert()
+        self.bang = pygame.image.load("Bang!.bmp").convert()
+        self.bang.set_colorkey((255,255,255))
     
-def paint_enemy(enemy_image,x,y):
-    screen.blit(enemy_image, (x,y))
+    def setup_enemies(self):
+        self.enemy1 = Enemy.Enemy(160,140,1000,3000,False,self.enemy_image)
+        self.enemy2 = Enemy.Enemy(351,140,2500,4500,False,self.enemy_image)
+        self.enemy3 = Enemy.Enemy(355,401,4000,6500,False,self.enemy_image)
+        self.enemy4 = Enemy.Enemy(200,401,6500,8000,False,self.enemy_image)
+        self.enemy5 = Enemy.Enemy(258,275,8500,10500,False,self.enemy_image)
+        self.enemy6 = Enemy.Enemy(460,527,10000,12000,False,self.enemy_image)
+        self.enemy7 = Enemy.Enemy(45,528,12500,13500,False,self.enemy_image)
+        self.enemy8 = Enemy.Enemy(255,530,13000,15000,False,self.enemy_image)
+        self.enemy9 = Enemy.Enemy(450,94,14500,16500,False,self.enemy_image)
+        self.enemy10 = Enemy.Enemy(62,94,16000,18000,False,self.enemy_image)
+        self.enemy11 = Enemy.Enemy(242,94,18800,20000,False,self.enemy_image)
+        self.enemies=[self.enemy1,self.enemy2,self.enemy3,self.enemy4,self.enemy5,self.enemy6,self.enemy7,self.enemy8,self.enemy9,self.enemy10,self.enemy11]
+
+    def strike_checker(self,mouse_click_coordinates, x_of_image, y_of_image):
+        if (mouse_click_coordinates[0] > x_of_image and mouse_click_coordinates[0] < x_of_image+50 and 
+            mouse_click_coordinates[1] > y_of_image and mouse_click_coordinates[1] < y_of_image + 70):
+            return True
     
-    
-def time_checker(time,born_time,death_time):
-    if(time > born_time and time < death_time):
-        return True
-      
-while running:
-    if pygame.event.peek():
-        event = pygame.event.poll()
-        if event.type == pygame.QUIT:
-            running = 0
+    def get_score(self):
+        highscore = shelve.open('highscore.db')
+        result=highscore.get('score')
+        highscore.close()
+        return result
+        
+        
+    def save_score(self):
+        if self.hits > self.get_score():
+            highscore = shelve.open('highscore.db')
+            highscore["score"] = self.hits
+            highscore.close()
             
-        screen.fill((0, 0, 0))
-        clear_screen()
-       
-        if (event.type == MOUSEBUTTONDOWN):
-            cords=pygame.mouse.get_pos()
-            for enemy in enemies:
-                if(time_checker(time,enemy.born,enemy.died) and not enemy.hit):
-                    enemy.hit=strike_checker(cords, enemy.x, enemy.y)
-                    if(enemy.hit):
-                        enemy.image=bang
-                        hits=hits+1
             
-        if (event.type == MOUSEBUTTONUP):
-            pressed_down = False
-            for enemy in enemies:
-                if enemy.hit:
-                    enemy.image=enemy_dead
+    def clear_screen(self):
+        self.screen.blit(self.background, (0,0))
+        
+    def paint_enemy(self,enemy_image,x,y):
+        self.screen.blit(enemy_image, (x,y))
+        
+        
+    def time_checker(self,time,born_time,death_time):
+        if(time > born_time and time < death_time):
+            return True
+            
+    
+    def show_text(self,font_name,text,x,y):
+        try:
+            font = pygame.font.Font(font_name, 25)
+        except Exception:
+            font = pygame.font.Font(None,25)
+        output = font.render(text, True, (255,255, 255), (159, 182, 205))
+        self.screen.blit(output, (x,y))
+        
+    def draw_mouse(self):
+        cordss=pygame.mouse.get_pos()
+        self.screen.blit(self.cursor,(cordss[0],cordss[1]))
+   
+    def main(self):
+        while self.running:
+            if pygame.event.peek():
+                if not pygame.mixer.get_busy():
+                    self.sound.play()
+                    
+                event = pygame.event.poll()
+                if event.type == pygame.QUIT:
+                    pygame.mouse.set_visible(1)
+                    pygame.mixer.stop()
+                    
+                    self.save_score()
+                    score=self.get_score()
+                    print(score)
+                    
+                    self.running = 0
                 
-        
-    time=time+clock.get_time();
-    if(time > 20001):
-        time=0
-        for enemy in enemies:
-            enemy.hit=False
-            enemy.image=enemy_image
+                self.screen.fill((0, 0, 0))
+                self.clear_screen()
+           
+                if (event.type == MOUSEBUTTONDOWN):
+                    
+                    cords=pygame.mouse.get_pos()
+                    for enemy in self.enemies:
+                        if(self.time_checker(self.time,enemy.born,enemy.died) and not enemy.hit):
+                            enemy.hit=self.strike_checker(cords, enemy.x, enemy.y)
+                            if(enemy.hit):
+                                self.sound_smash.play()
+                                enemy.image=self.bang
+                                self.hits=self.hits+1
+                
+                if (event.type == MOUSEBUTTONUP):
+                    
+                    self.pressed_down = False
+                    for enemy in self.enemies:
+                        if enemy.hit:
+                            enemy.image=self.enemy_dead
+                    
             
-    for enemy in enemies: 
-        if(time_checker(time,enemy.born,enemy.died)):
-            paint_enemy(enemy.image,enemy.x,enemy.y)
-        if(time >= enemy.died):
-            clear_screen()
-            
-    font = pygame.font.Font('Fonter.ttf', 25)
-    printer='tohki: ' +str(hits)
-    text = font.render(printer, True, (255,255, 255), (159, 182, 205))
-    screen.blit(text, (5,5))
-    cursor.set_colorkey((180,180,180))
-    cordss=pygame.mouse.get_pos()
-    screen.blit(cursor,(cordss[0],cordss[1]))
-    clock.tick(60) 
-    pygame.display.update()
+            self.time=self.time+self.clock.get_time();
+            if(self.time > 20001):
+                self.time=0
+                for enemy in self.enemies:
+                    enemy.hit=False
+                    enemy.image=self.enemy_image
+                
+            for enemy in self.enemies: 
+                if(self.time_checker(self.time,enemy.born,enemy.died)):
+                    self.paint_enemy(enemy.image,enemy.x,enemy.y)
+                if(self.time >= enemy.died):
+                    self.clear_screen()
+                
+            output='tohki: ' +str(self.hits)
+            self.show_text('Fonter.ttf',output,5,5)
+            self.draw_mouse()
+            self.clock.tick(60) 
+            pygame.display.update()
